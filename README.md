b25babfba89a83094c0c06fe0965e786d1931393
dineshnp03 <dineshnpcan@gmail.com>
2024-12-13 16:14:33 -0500

    final changes before submission

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PHP-Team
+
+
+Need to run the dbSetup.php file, to load the products and category initially.
+
+For Admin Portal, desimart/admin - to load the admin portal 
+
+For CSP, desimart/login.php - to load the CSP 
--- desimart/admin/includes/header.php
+++ desimart/admin/includes/header.php
@@ -9,7 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title>Admin Portal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/styles.css">
@@ -20,7 +20,7 @@ if (session_status() === PHP_SESSION_NONE) {
         <nav>
             <div class="d-flex align-items-center">
                 <a class="bg-none" href="./dashboard.php"><img class="logo" src="../images/logo.jpeg" alt="Company logo" /></a>
-                <h1 class="ms-2">Admin Panel</h1>
+                <h1 class="ms-2">Admin Portal</h1>
             </div>
             <div>
                 <?php if (isset($_SESSION['user_id']) && $_SESSION['role'] === 'admin'): ?>
